a0ef0f1c1b7697880f1aa556202bc19318df6022
Abdullah Akram Ansari <abdullaha@nulogy.com>
2020-05-23 20:02:41 -0400

    Got the view part working, now want to finish up the update part

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
-var express = require('express');
-var router = express.Router();
+const { MongoClient, ObjectID } = require('mongodb');
+const express = require('express');
+const router = express.Router();
+
+const uri = "mongodb://localhost:27017/";
+const client = new MongoClient(uri);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render("index");
+});
+
+router.post('/search', function(req, res, next) {
+  const searchString = req.body.citySearchString;
+  const regexp = new RegExp(`${searchString}`, 'i')
+
+  client.connect(function(err) {
+    const db = client.db("banDB");
+    const collection = db.collection('cities');
+
+    collection.find({ city: regexp }).limit(10).toArray(function(err, docs) {
+      if (docs !== undefined) {
+        console.log(docs);
+        console.log(docs.length);
+      }
+      res.render("find", { cities: docs, searchString: searchString });
+    });
+  });
+});
+
+router.get("/view/:cityId", function(req, res, next) {
+  const cityId = req.params.cityId;
+
+  client.connect(function(err) {
+    const db = client.db("banDB");
+    const collection = db.collection('cities');
+
+    collection.findOne({ _id: ObjectID(cityId) }, {}, function(err, doc) {
+      res.render("view", { city: doc });
+    });
+  });
 });
 
 module.exports = router;
--- scripts/find.js
+++ scripts/find.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectID } = require('mongodb');
 const assert = require('assert');
 
 const uri = "mongodb://localhost:27017/";
@@ -10,9 +10,9 @@ client.connect(function(err) {
     console.log("Connected successfully to server");
 
     const db = client.db("banDB");
-    const collection = db.collection('lobsters');
+    const collection = db.collection('cities');
 
-    collection.find({'Primary Phone': "3014233715"}).toArray(function(err, docs) {
+    collection.find({'_id': ObjectID("5ec9603eeb5f38d1de72c93e")}).toArray(function(err, docs) {
         assert.equal(err, null);
         console.log("Found the following records");
         console.log(docs);
