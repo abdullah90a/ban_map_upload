0b63fcd6835936fef339188a990b3fc07f3f690a
Abdullah Akram Ansari <abdullaha@nulogy.com>
2020-06-06 11:43:21 -0400

    Implements deletion F/E deletion functionality.

--- public/javascripts/mapScript.js
+++ public/javascripts/mapScript.js
@@ -1,3 +1,7 @@
+let MARKERS = [];
+let MAP_DATA = undefined;
+let SELECTED_TYPE = undefined;
+
 const MAP = L.map('map').setView([38, -95], 4);
 L.tileLayer.provider('CartoDB.Positron').addTo(MAP);
 
@@ -11,7 +15,6 @@ info.onAdd = function () {
 info.addTo(MAP);
 
 const TYPE_SELECTOR = document.getElementById('typeSelect');
-let SELECTED_TYPE = undefined;
 TYPE_SELECTOR.addEventListener('change', evt => {
     SELECTED_TYPE = evt.target.value;
     getTypeData();
@@ -24,13 +27,12 @@ function getTypeData() {
     request.send();
 }
 
-let MARKERS = [];
 function mapData() {
-    const data = JSON.parse(this.responseText);
+    MAP_DATA = JSON.parse(this.responseText);
 
     MARKERS.forEach(marker => marker.remove());
     MARKERS = [];
-    data.forEach(datum => {
+    MAP_DATA.forEach(datum => {
         const marker = L.marker([datum.lat, datum.lng], {
             icon: L.icon({
                 iconUrl: `http://127.0.0.1:3000/markers/${SELECTED_TYPE}.png`,
@@ -51,5 +53,49 @@ function mapData() {
 
         marker.addTo(MAP);
         MARKERS.push(marker);
-    })
+    });
+
+    createTypeAndItemsDisplay();
+}
+
+function deleteType() {
+    MARKERS.forEach(marker => marker.remove());
+
+    const request = new XMLHttpRequest();
+    request.onload = () => window.location.reload();
+    request.open('GET', `http://127.0.0.1:3000/nuUpload/deleteType/${SELECTED_TYPE}`);
+    request.send();
+}
+
+function deleteItem(index, id) {
+    MARKERS[index].remove();
+
+    const request = new XMLHttpRequest();
+    request.onload = () => getTypeData();
+    request.open('GET', `http://127.0.0.1:3000/nuUpload/deleteItem/${SELECTED_TYPE}/${id}`);
+    request.send();
+}
+
+const DISPLAY_DIV = document.getElementById('types');
+function createTypeAndItemsDisplay() {
+    let items_list = '';
+    MAP_DATA.forEach((datum, index) => {
+        let item = '';
+        const keysToRender = Object.keys(datum).filter(key => !(['_id', 'lat', 'lng'].includes(key)));
+        keysToRender.forEach(key => {
+            item += `<tr><td>${key}</td><td>${datum[key]}</td></tr>`;
+        });
+        items_list += `
+            <li>
+                ${'<table>' + item + '</table>'}
+                <button onclick="deleteItem(${index + ", '" + datum["_id"] + "'"})">Delete</button>
+            </li>
+        `;
+    });
+
+    DISPLAY_DIV.innerHTML = `
+        <h3>Items in '${SELECTED_TYPE}'</h3>
+        <button onclick="deleteType()">Delete</button>
+        <ul>${items_list}</ul>
+    `;
 }
